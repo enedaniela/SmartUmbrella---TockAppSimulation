c2371d9e6949b95d86d5b32f605eb3ef95453a7b
Daniela Ene <danielaene84@gmail.com>
2021-01-05 21:20:13 +0200

    Add project files.

---
+++ SmartUmbrella/Program.cs
@@ -0,0 +1,81 @@
+﻿// Use this code inside a project created with the Visual C# > Windows Desktop > Console Application template.
+// Replace the code in Program.cs with this code.
+
+using System;
+using System.IO.Ports;
+using System.Threading;
+
+public class PortChat
+{
+    static bool _continue;
+    static SerialPort _serialPort;
+
+    public static void Main()
+    {
+        string name;
+        string message;
+        StringComparer stringComparer = StringComparer.OrdinalIgnoreCase;
+        Thread readThread = new Thread(Read);
+
+        // Create a new SerialPort object with default settings.
+        _serialPort = new SerialPort();
+
+        // Set the appropriate properties.
+        _serialPort.PortName = "COM2";
+        _serialPort.BaudRate = 9600;
+        _serialPort.Parity = Parity.None;
+        _serialPort.DataBits = 8;
+        _serialPort.StopBits = StopBits.One;
+        _serialPort.Handshake = Handshake.None;
+
+        // Set the read/write timeouts
+        _serialPort.ReadTimeout = 500;
+        _serialPort.WriteTimeout = 500;
+
+        _serialPort.Open();
+        _continue = true;
+        readThread.Start();
+
+        name = "Tock application";
+
+        Console.WriteLine("In Tock application. Type QUIT to exit");
+
+        while (_continue)
+        {
+            message = Console.ReadLine();
+
+            if (stringComparer.Equals("quit", message))
+            {
+                _continue = false;
+            }
+            else
+            {
+                _serialPort.WriteLine(
+                    String.Format("{0}",message));
+            }
+        }
+
+        readThread.Join();
+        _serialPort.Close();
+    }
+
+    public static void Read()
+    {
+        while (_continue)
+        {
+            try
+            {
+                string message = _serialPort.ReadLine();
+                if(message == "0")
+                    Console.WriteLine("Response from Weather Service: Not going to rain");
+                else if (message == "1")
+                    Console.WriteLine("Response from Weather Service: Rain in the forecast. LED on.");
+                else
+                {
+                    Console.Write("Unknown message: " + message);
+                }
+            }
+            catch (TimeoutException) { }
+        }
+    }
+}
